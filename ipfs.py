3a5d78d09ed2d46eb5067d7d1e3f4bea619d93e9
andrewlin1368 <andrewlin1368@gmail.com>
2021-05-13 13:35:05 -0400

    cleared

--- ipfs.py
+++ ipfs.py
@@ -13,11 +13,10 @@ filesystem = FileServer()
 class Blockchain:
     def __init__(self):
         self.data = []
-        self.chain = []
         self.fat = []
+        self.chain = []
         self.nodes = set()
         self.new_block(previous_hash='0')
-        self.update_block()
         self.create_nodes(address='http://127.0.0.1:5000')
         self.create_nodes(address='http://127.0.0.1:5001')
         self.create_nodes(address='http://127.0.0.1:5002')
@@ -32,48 +31,6 @@ class Blockchain:
         else:
             raise ValueError('Invalid')
 
-    def update_block(self):
-        self.data = filesystem.get_data()
-        for i in self.data:
-            if i[1] != -2:
-                self.fat.append(i[1])
-        self.new_fat_log()
-
-    def new_request(self, data, location, sender_port):
-        self.data = filesystem.get_data()
-        next_location = self.data[location][1]
-        self.data[location] = [data, next_location]
-        for i in self.data:
-            if i[1] != -2:
-                self.fat.append(i[1])
-        network = self.nodes
-        for node in network:
-            if node != sender_port:
-                response = requests.get(f'http://{sender_port}/get/fat')
-                if response.status_code == 200:
-                    requests.post(f'http://{node}/update/fat', json={
-                        'fat': response.json()['fat']
-                    })
-
-        network = self.nodes
-        counter = 0
-        for node in network:
-            if node != sender_port:
-                response = requests.post(f'http://{node}/validate/fat', json={})
-                if response.status_code == 200:
-                    counter += 1
-
-        if self.consensus(counter, sender_port) is True:
-            network = self.nodes
-            response = requests.get(f'http://{sender_port}/get/fat')
-            if response.status_code == 200:
-                for node in network:
-                    requests.post(f'http://{node}/final/fat', json={'fat': response.json()['fat']})
-                    requests.post(f'http://{node}/add/block', json={})
-
-        filesystem.edit_data(data,location)
-        print(filesystem.data)
-
     def consensus(self, counter, sender_port):
         total_nodes = 0
         network = self.nodes
@@ -84,29 +41,6 @@ class Blockchain:
             return True
         return False
 
-    def validate_fat(self):
-        self.data = filesystem.get_data()
-        check = []
-        for i in self.data:
-            if i[1] != -2:
-                check.append(i[1])
-        if check == self.fat:
-            return True
-        return False
-
-    def store_fat(self, fats):
-        self.fat = fats
-
-    def final_fat(self, fats):
-        self.fat = fats
-
-    # create fat logs
-    def new_fat_log(self):
-        if self.validate() is True:
-            last_block = self.last_block
-            previous_hash = self.hash(last_block)
-            self.new_block(previous_hash)
-
     def new_block(self, previous_hash):
         block = {
             'index': len(self.chain) + 1,
@@ -114,7 +48,6 @@ class Blockchain:
             'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
         self.fat = []
-        self.data = []
         self.chain.append(block)
         return block
 
@@ -143,76 +76,6 @@ class Blockchain:
 app = Flask(__name__)
 node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
-
-
-@app.route('/add/block', methods=['POST'])
-def add_block():
-    blockchain.new_fat_log()
-    return jsonify(), 201
-
-
-@app.route('/new/request', methods=['POST'])
-def new_request():
-    values = request.get_json()
-    required = ['data', 'location', 'sender_port']
-    if not all(keys in values for keys in required):
-        return 'Missing request info', 400
-    blockchain.new_request(values['data'], values['location'], values['sender_port'])
-    response = {'message': "REQUEST DONE"}
-    return jsonify(response), 201
-
-
-# get chain
-@app.route('/get/chain', methods=['GET'])
-def get_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain)
-    }
-    return jsonify(response), 200
-
-
-@app.route('/get/data', methods=['GET'])
-def get_data():
-    response = {
-        'data': blockchain.data
-    }
-    return jsonify(response), 200
-
-
-@app.route('/get/fat', methods=['GET'])
-def get_fat():
-    response = {
-        'fat': blockchain.fat
-    }
-    return jsonify(response), 200
-
-
-@app.route('/update/fat', methods=['POST'])
-def update_fat():
-    values = request.get_json()
-    required = ['fat']
-    if not all(keys in values for keys in required):
-        return 'Missing request info', 400
-    blockchain.store_fat(values['fat'])
-    response = {'message': "UPDATE DONE"}
-    return jsonify(response), 201
-
-
-@app.route('/validate/fat', methods=['POST'])
-def validate_fat():
-    blockchain.validate()
-    return jsonify(), 200
-
-
-@app.route('/final/fat', methods=['POST'])
-def final_fat():
-    values = request.get_json()
-    required = ['fat']
-    if not all(keys in values for keys in required):
-        return 'Missing request info', 400
-    blockchain.final_fat(values['fat'])
-    return jsonify(), 200
 
 
 if __name__ == '__main__':
