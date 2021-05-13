4fd1e1edbb185eaea8eda53604070be9c94ad217
andrewlin1368 <andrewlin1368@gmail.com>
2021-05-13 13:34:49 -0400

    cleared

--- fs.py
+++ fs.py
@@ -1,12 +1,1 @@
-class FileServer:
-    def __init__(self):
-        self.data = [['A', 1], ['B', 3], ['Null', -2], ['C', 4], ['D', -1]]
 
-    def get_data(self):
-        return self.data
-
-    def edit_data(self, new_data, location):
-        self.data[location] = [new_data, self.data[location][1]]
-
-
-filesystem = FileServer()
